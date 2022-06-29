54632f6caaf48a387af5d2689c81ffb7ad0237e4
MOST <handsome010216@gmail.com>
2022-06-30 05:53:22 +0900

    redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Counter } from "./features/counter/Counter";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+import { Navbar } from "./app/Navbar";
+import { PostsList } from "./features/posts/PostsList";
+import { AddPostForm } from "./features/posts/AddPostForm";
+import { SinglePostPage } from "./features/posts/SinglePostPage";
+import { EditPostForm } from "./features/posts/EditPostForm";
+
 function App() {
   return (
-    <div className="App">
-      <Counter />
-    </div>
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Routes>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <React.Fragment>
+                <AddPostForm />
+                <PostsList />
+              </React.Fragment>
+            )}
+          />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
+          {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import counterReducer from "../features/counter/counterSlice.js";
+import postsReducer from "../features/posts/postsSlice";
+import usersReducer from "../features/users/usersSlice";
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    posts: postsReducer,
+    users: usersReducer,
   },
 });
